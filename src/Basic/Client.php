8b475e3d4c7f2b64c7c9b3415e7c91da172cfb2d
sting <sting_bo@163.com>
2021-03-14 20:40:07 +0800

    add /api/%s/add Add a file to ipfs.

--- src/Basic/Client.php
+++ src/Basic/Client.php
@@ -9,15 +9,31 @@ class Client extends BaseClient
     /**
      * Show ipfs node id info.
      *
-     * @param $get_params
+     * @param array $query
      *
      * @return array|\EasyIPFS\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
      *
      * @throws \EasyIPFS\Kernel\Exceptions\InvalidConfigException
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function id($get_params = [])
+    public function id($query = [])
     {
-        return $this->httpPost('/api/%s/id', $get_params);
+        return $this->httpPost('/api/%s/id', $query);
+    }
+
+    /**
+     * Add a file to ipfs.
+     *
+     * @param array $files
+     * @param array $query
+     *
+     * @return array|\EasyIPFS\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyIPFS\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function add(array $files, $query = [])
+    {
+        return $this->httpUpload('/api/%s/add', $files, $query);
     }
 }
