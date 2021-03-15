f4de2a371229afe3ee761f706cffd7beb3da72c4
sting <sting_bo@163.com>
2021-03-15 09:13:28 +0800

    update function sort

--- src/Basic/Client.php
+++ src/Basic/Client.php
@@ -7,36 +7,6 @@ use EasyIPFS\Kernel\BaseClient;
 class Client extends BaseClient
 {
     /**
-     * Show ipfs node id info.
-     *
-     * @param array $query
-     *
-     * @return array|\EasyIPFS\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
-     *
-     * @throws \EasyIPFS\Kernel\Exceptions\InvalidConfigException
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    public function id($query = [])
-    {
-        return $this->httpPost('/api/%s/id', $query);
-    }
-
-    /**
-     * Download IPFS objects.
-     *
-     * @param array $query
-     *
-     * @return string
-     *
-     * @throws \EasyIPFS\Kernel\Exceptions\InvalidConfigException
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    public function get($query = [])
-    {
-        return $this->requestRaw('/api/%s/get', 'POST', ['query' => $query])->getBodyContents();
-    }
-
-    /**
      * Add a file to ipfs.
      *
      * @param array $files
@@ -65,6 +35,36 @@ class Client extends BaseClient
     public function cat($query = [])
     {
         return $this->requestRaw('/api/%s/cat', 'POST', ['query' => $query])->getBodyContents();
+    }
+
+    /**
+     * Download IPFS objects.
+     *
+     * @param array $query
+     *
+     * @return string
+     *
+     * @throws \EasyIPFS\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function get($query = [])
+    {
+        return $this->requestRaw('/api/%s/get', 'POST', ['query' => $query])->getBodyContents();
+    }
+
+    /**
+     * Show ipfs node id info.
+     *
+     * @param array $query
+     *
+     * @return array|\EasyIPFS\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyIPFS\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function id($query = [])
+    {
+        return $this->httpPost('/api/%s/id', $query);
     }
 
     /**
